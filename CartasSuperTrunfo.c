a816fa4030ae7420cf573a190439d977e8d42708
Xilambila <edinhodmf@gmail.com>
2025-02-16 01:49:20 +0000

    Codado o Arquivo, em conjunto com os comentarios do arquivo Principal CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -17,6 +17,39 @@ int main() {
     // Exibição dos Dados das Cartas:
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    
+    //Supertrunfo BRASIL - Pais escolhido para o desafio Iniciante
+    int populacao, p_turisticos, cidade;
+    float area, pib;
+    char estado;
 
+    printf("Cadastro de cartas do Super Trunfo - Paises\n"); //Abaixo no codigo esta configurado o Cadastro de cartas do Super Trunfo
+
+    printf("Digite a Letra do Estado (A-H): ");
+    scanf(" %c", &estado);
+
+    printf("Digite o numero da cidade (01-04): ");
+    scanf("%d", &cidade);
+
+    printf("Populacao: ");
+    scanf("%d", &populacao);
+
+    printf("Area (km²): ");
+    scanf("%f", &area);
+
+    printf("PIB da cidade (em Bilhoes): ");
+    scanf("%f", &pib);
+
+    printf("Numero de pontos turisticos: ");
+    scanf("%d", &p_turisticos);
+
+    //A partir deste ponto, exibimos os dados da carta computada.
+    printf ("------------------------------------------------------\n");
+    printf ("Codigo da Carta %c%.2d\n", estado, cidade);
+    printf ("Populacao: %d\n", populacao);
+    printf ("Area: %.2f km²\n", area);
+    printf ("PIB: %.2f Bilhoes\n", pib);
+    printf ("Pontos Turisticos: %d\n", p_turisticos);
+    printf ("------------------------------------------------------\n");
     return 0;
 }
